516d1ded886fefc6dfbd8c92ce3f225af1502663
Ygao-Underground <ygaounderground011@gmail.com>
2024-12-06 01:35:43 -0300

    Diretivas v-if e v-show

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
-  <TheHeader />
-  <img alt="Vue logo" src="./assets/logo.png">
+  <TheHeader
+    v-if="showHeader"
+  />
+
+  <div v-show="showName">
+      Nome: {{ firstName }} <br>
+      Sobrenome: {{ lastName }}
+  </div>
+
+  <div v-if="accessLevel === 'admin'">Admin</div>
+  <div v-else-if="accessLevel === 'marketing'">Marketing</div>
+  <div v-else>User</div>
+
+  <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+  >
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -14,6 +29,15 @@ export default {
   components: {
     HelloWorld,
     TheHeader
+  },
+  data() {
+    return {
+      showHeader: true,
+      firstName: 'Ygao ',
+      lastName: 'Underground',
+      showName: false,
+      accessLevel: 'admin'
+    }
   }
 }
 </script>
